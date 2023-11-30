825892f2c9fc1f3bc9cdd80f92e6976a506d92f2
jmatheis00 <jmatheis@student.42heilbronn.de>
2023-11-30 17:09:25 +0100

    mariadb working, but problems with connecting with wordpress

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -5,21 +5,31 @@ echo "DB_PASS: $DB_PASS"
 echo "DB_NAME: $DB_HOSTNAME"
 echo "DB_ROOT: $DB_ROOTPASS"
 
+# Initialize MariaDB
+
+# Performing root user setup
+# echo "Root user setup: mysql -u root -p${DB_ROOTPASS} --socket=/var/run/mysqld/mysqld.sock -e \"ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOTPASS';\""
+# mysql -u root -p"${DB_ROOTPASS}" --socket=/var/run/mysqld/mysqld.sock -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOTPASS';"
+
+# Perform database setup
 if [ ! -d "/var/lib/mysql/$DB_DATABASE" ]; then
     service mariadb start
     sleep 4
-    echo "Creating wordpress database..."
+    echo "Creating database and user..."
     mysql -u root -p"${DB_ROOTPASS}" <<EOF
     CREATE DATABASE IF NOT EXISTS ${DB_DATABASE};
     CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
     GRANT ALL PRIVILEGES ON ${DB_DATABASE}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
-    ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOTPASS';
     FLUSH PRIVILEGES;
 EOF
-    echo "$DB_DATABASE Database created successfully!"
-    # sleep 2
-    # mysqladmin --user=root --password=$DB_ROOTPASS shutdown
+    echo "Database setup completed successfully."
+    sleep 2
+    service mariadb stop
+else
+    echo "Database already exists. Skipping setup."
 fi
 
-echo "Outside of statement"
-exec mysqld
+# Clean up
+
+# Additional logging
+exec "mysqld_safe"
--- srcs/requirements/wordpress/conf/create_wp.sh
+++ srcs/requirements/wordpress/conf/create_wp.sh
@@ -6,51 +6,33 @@ echo $DB_USER
 echo $DB_PASS
 echo $DB_HOSTNAME
 
-####### MANDATORY PART ##########
-# Create the PHP-FPM error log file if it doesn't exist
-if [ ! -f /var/log/php-fpm/error.log ]; then
-    mkdir -p /var/log/php-fpm
-    touch /var/log/php-fpm/error.log
-    chown -R www-data:www-data /var/log/php-fpm
-fi
+# Wait for the MySQL server to be ready
+# echo "Waiting for MariaDB server to be ready..."
+# until mysql --host=mariadb --user="$DB_USER" --password="$DB_PASS" -e '\c'; do
+#     echo "MariaDB is not ready yet. Retrying in 3 seconds..."
+#     sleep 1
+# done
+
+echo "MariaDB server is ready. Proceeding with WordPress setup."
 
 cd /var/www/html
 # Download WordPress
-wp core download --path=/var/www/html --allow-root
+wp-cli core download --path=/var/www/html --allow-root
 
 # Copy the sample config file
-# cp wp-config-sample.php wp-config.php
+cp wp-config-sample.php wp-config.php
+
+# Use sed to replace placeholder values with actual database details
+sed -i "s/'database_name_here'/'$DB_DATABASE'/g" wp-config.php
+sed -i "s/'username_here'/'$DB_USER'/g" wp-config.php
+sed -i "s/'password_here'/'$DB_PASS'/g" wp-config.php
+sed -i "s/'localhost'/'$DB_HOSTNAME'/g" wp-config.php
 
 # Remove existing directories
-wp core is-installed --allow-root || wp core install --url=http://localhost --title="Inception Project jmatheis" \
+wp-cli core is-installed --allow-root || wp-cli core install --allow-root --url=http://localhost --title="Inception Project jmatheis" \
 	--admin_user=jmatheis --admin_password=wordpress --admin_email=jmatheis@student.42heilbronn.de --allow-root
-wp core config --dbname="$DB_DATABASE" --dbuser="$DB_USER" --dbpass="$DB_PASS" --dbhost="$DB_HOSTNAME" --extra-php <<PHP
+wp-cli core config --allow-root --dbname="$DB_DATABASE" --dbuser="$DB_USER" --dbpass="$DB_PASS" --dbhost="$DB_HOSTNAME" --extra-php <<PHP
 define( 'WP_DEBUG', false );
 PHP
 
-# Install WordPress
-# wp core install --url=http://localhost --title="Inception Project jmatheis" --admin_user=jmatheis --admin_password=wordpress --admin_email=jmatheis@student.42heilbronn.de --allow-root
-# 	#Download wordpress and all config file
-# 	wget http://wordpress.org/latest.tar.gz
-# 	tar xfz latest.tar.gz
-# 	# Remove existing directories
-# 	rm -rf ./wp-admin
-# 	rm -rf ./wp-content
-# 	rm -rf ./wp-includes
-# 	mv wordpress/* .
-# 	rm -rf latest.tar.gz
-# 	rm -rf wordpress
-
-# 	# Copy the sample config file
-#     cp wp-config-sample.php /var/www/html/wp-config.php
-# 	#Inport env variables in the config file
-# 	sed -i "s/wordpress/$DB_DATABASE/g" /var/www/html/wp-config.php
-# 	sed -i "s/jmatheis/$DB_USER/g" /var/www/html/wp-config.php
-# 	sed -i "s/pass/$DB_PASS/g" /var/www/html/wp-config.php
-# 	sed -i "s/mysql/$DB_HOSTNAME/g" /var/www/html/wp-config.php
-# ###################################
-# fi
-
 exec "$@"
-
-# The wp-config.php file you provided has placeholder values for the database configuration, which means it hasn't been configured with the actual database details. Update the following lines with the correct values:
