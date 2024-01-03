068ed88709a276faf034aac1f84f0bd9448c3c02
jmatheis00 <jmatheis@student.42heilbronn.de>
2024-01-03 11:24:41 -0500

    should work now

--- srcs/requirements/wordpress/conf/create_wp.sh
+++ srcs/requirements/wordpress/conf/create_wp.sh
@@ -4,21 +4,21 @@ echo $DB_DATABASE
 echo $WP_ADMINUSER
 echo $WP_ADMIN_MAIL
 
-cd /var/www/html
 
 # Create Wordpress Admin, 
-if ! wp-cli core is-installed --allow-root; then
+if ! wp-cli core is-installed --path=/var/www/html --allow-root; then
 	# Download WordPress core files without installing or configuring them
 	wp-cli core download --path=/var/www/html --allow-root
-	# # Copy the sample config file
+	# Copy the sample config file
+	cd /var/www/html
 	cp wp-config-sample.php wp-config.php
 
-	# # edit wp-config.php file, replacing placeholder values
+	# edit wp-config.php file, replacing placeholder values
 	sed -i "s/'database_name_here'/'$DB_DATABASE'/g" wp-config.php
 	sed -i "s/'username_here'/'$DB_USER'/g" wp-config.php
 	sed -i "s/'password_here'/'$DB_PASS'/g" wp-config.php
 	sed -i "s/'localhost'/'$DB_HOSTNAME'/g" wp-config.php
-	# # core install completes the installation
+	# core install completes the installation
 	wp-cli core install --url=$DOMAIN_NAME --title=$WP_TITLE \
 		--admin_user=$WP_ADMINUSER --admin_password=$WP_ADMINPASS \
 		--admin_email=$WP_ADMINMAIL --allow-root
